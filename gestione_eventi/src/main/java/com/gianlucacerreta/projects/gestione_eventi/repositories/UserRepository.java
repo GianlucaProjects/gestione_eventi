760413540eb3d682811a57255f4796395fd0df8c
Gianluca Cerreta <gianluca2003ba@gmail.com>
2024-11-08 11:31:42 +0100

    creato repository e service

---
+++ gestione_eventi/src/main/java/com/gianlucacerreta/projects/gestione_eventi/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gianlucacerreta.projects.gestione_eventi.repositories;
+
+
+import com.gianlucacerreta.projects.gestione_eventi.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
