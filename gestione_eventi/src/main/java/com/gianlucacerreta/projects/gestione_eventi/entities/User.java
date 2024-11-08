89b11fd150ff8d14a32b521ff5349fa675d54d14
Gianluca Cerreta <gianluca2003ba@gmail.com>
2024-11-08 11:19:48 +0100

    aggiunti entity e enum

---
+++ gestione_eventi/src/main/java/com/gianlucacerreta/projects/gestione_eventi/entities/User.java
@@ -0,0 +1,25 @@
+package com.gianlucacerreta.projects.gestione_eventi.entities;
+
+import com.gianlucacerreta.projects.gestione_eventi.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
---
+++ gestione_eventi/src/main/java/com/gianlucacerreta/projects/gestione_eventi/enums/Role.java
@@ -0,0 +1,6 @@
+package com.gianlucacerreta.projects.gestione_eventi.enums;
+
+public enum Role {
+    USER,
+    ORGANIZZATORE_DI_EVENTI
+}
