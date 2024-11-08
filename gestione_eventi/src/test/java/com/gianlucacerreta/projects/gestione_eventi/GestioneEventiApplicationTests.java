a185535a5096202a37f1d782999da20f0222c56b
Gianluca Cerreta <gianluca2003ba@gmail.com>
2024-11-08 11:12:32 +0100

    aggiornato .gitignore

---
+++ gestione_eventi/src/test/java/com/gianlucacerreta/projects/gestione_eventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianlucacerreta.projects.gestione_eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
