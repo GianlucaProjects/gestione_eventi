cb25460fec8e3089f1c995a5264e3ee70d527588
Gianluca Cerreta <gianluca2003ba@gmail.com>
2024-11-08 17:01:03 +0100

    ultime modifiche

---
+++ gestione_eventi/src/main/java/com/gianlucacerreta/projects/gestione_eventi/auth/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.gianlucacerreta.projects.gestione_eventi.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+}
+
---
+++ gestione_eventi/src/main/java/com/gianlucacerreta/projects/gestione_eventi/auth/JwtTokenProvider.java
@@ -0,0 +1,57 @@
+package com.gianlucacerreta.projects.gestione_eventi.auth;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final Key key;
+
+    private final long jwtExpirationInMs = 864000;
+
+    public JwtTokenProvider(@Value("${app.jwtSecret}") String jwtSecret) {
+        this.key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromJWT(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(authToken);
+            return true;
+        } catch (JwtException ex) {
+            return false;
+        }
+    }
+}
+
