a24b981caa84c98accb5dcb8942286a6f6031344
Gianluca Cerreta <gianluca2003ba@gmail.com>
2024-11-08 11:47:01 +0100

    Aggiunto SecurityConfig

---
+++ gestione_eventi/src/main/java/com/gianlucacerreta/projects/gestione_eventi/auth/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.gianlucacerreta.projects.gestione_eventi.auth;
+
+import com.gianlucacerreta.projects.gestione_eventi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity // Sostituisce @EnableGlobalMethodSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private UserService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/events/create", "/events/edit/**", "/events/delete/**").hasRole("ORGANIZER")
+                        .requestMatchers("/events/**", "/bookings/**").hasAnyRole("USER", "ORGANIZER")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(item -> item.disable())
+                .logout(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
